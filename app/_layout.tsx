a57cd78ac6f411afa562f46bb55fd1de238531b7
Prakhar Sahu <prakhars762@gmail.com>
2025-08-17 15:29:09 +0530

    tab navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,25 @@ import { SplashScreen, Stack } from "expo-router";
 import "./globals.css";
 import { useEffect } from "react";
 import { useFonts } from 'expo-font';
+import * as Sentry from '@sentry/react-native';
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: 'https://4b45dd1e3c65b1b01f197fdf3efb4d14@o4509858705768448.ingest.us.sentry.io/4509858708258816',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
   const [fontsLoaded, error] = useFonts({
     "QuickSand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
     "QuickSand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
@@ -18,4 +35,4 @@ export default function RootLayout() {
   }, [fontsLoaded, error]);
 
   return <Stack screenOptions={{ headerShown: false }} />;
-}
+});
